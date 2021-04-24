57cc52e044b3512090c4ffe6d2108c29c6f4e856
lborgesc <lborgesc@outlook.com>
2021-04-24 09:05:38 -0300

    frist commit

---
+++ src/pages/UserIdentification.tsx
@@ -0,0 +1,156 @@
+import React, { useState } from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Platform,
+    Keyboard,
+    Alert,
+} from 'react-native';
+
+import { Button } from '../components/Button';
+import { useNavigation } from '@react-navigation/core';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+
+export function UserIdentification() {
+    const [isFocused, setIsFocuses] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+    const [name, setName] = useState<string>();
+
+    const navigation = useNavigation();
+ 
+    async function handleSubmit(){
+        if (!name)
+            return Alert.alert('Me diz como chamar você 🥺');
+
+        try{
+            await AsyncStorage.setItem('@plantmanager:user', name);
+            navigation.navigate('Confirmation',{
+                title: 'Prontinho',
+                subtitle: ' Agora vamos começar a cuidar das suas plantinhas com muito cuidado.',
+                buttonTitle: 'Começar',
+                icon: 'smile',
+                nextScreen: 'PlantSelect',
+            }); 
+        }catch{
+            return Alert.alert('Não foi possível salvar o seu nome 🥺');
+        }
+
+    }
+
+    function handleInputBlur(){
+        setIsFocuses(false);
+        setIsFilled(!!name);
+    }   
+
+    function handleInputFocus(){
+        setIsFocuses(true);
+    }
+
+    function handleInputChange(value: string){
+        setIsFilled(!!value);
+        setName(value);
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <KeyboardAvoidingView 
+                style={styles.container}
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            >
+
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+
+                            <View style={styles.header}>
+                                <Text style={styles.emoji}>
+                                    {isFilled ? '😄' : '😀'}
+                                </Text>
+
+                                <Text style={styles.title}>
+                                    Como podemos{`\n`}
+                                    chamar você?
+                                </Text>
+                            </View>
+                            <TextInput
+                                style={[
+                                    styles.input,
+                                    (isFocused || isFilled) && { borderColor: colors.green}
+                                ]}
+                                placeholder={'Digite um nome'}
+                                onBlur={handleInputBlur}
+                                onFocus={handleInputFocus}
+                                onChangeText={handleInputChange}
+                            />
+
+                            <View style={styles.footer}>
+                                <Button
+                                    title='Confirmar'
+                                    onPress={handleSubmit}
+                                />
+                            </View>
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent:'space-around',
+    },
+    content:{
+        flex: 1,
+        width: '100%',
+    },
+    form:{ 
+        flex: 1,
+        alignItems: 'center',
+        justifyContent:'center',
+        paddingHorizontal: 54,
+    },
+    header:{
+        alignItems: 'center',
+    },
+    emoji:{
+        fontSize: 40,
+    },
+    title:{
+        fontSize: 24,
+        textAlign: 'center',
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        lineHeight:32,
+        marginTop:20,
+        },
+    input:{
+        borderBottomWidth: 1,
+        borderColor: colors.gray,
+        color: colors.heading,
+        width: '100%',
+        fontSize: 18,
+        marginTop: 50,
+        padding: 10, 
+        textAlign: 'center',
+    },
+    footer:{
+        width: '100%',
+        marginTop: 40,
+        paddingHorizontal: 20,
+    }
+
+})
---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';       
+
+import { Welcome } from '../pages/Welcome';
+import { UserIdentification } from '../pages/UserIdentification';
+import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
+import { PlantSave } from '../pages/PlantSave';
+import { MyPlants } from '../pages/MyPlants';
+
+import colors from '../styles/colors';
+
+
+
+const StackRoutes = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+    <StackRoutes.Navigator 
+        headerMode='none'
+        screenOptions={{
+            cardStyle:{
+                backgroundColor: colors.white
+            },
+        }}
+    >
+        <StackRoutes.Screen
+            name="Welcome"
+            component={Welcome}
+        />
+        <StackRoutes.Screen
+            name="UserIdentification"
+            component={UserIdentification}
+        />
+        <StackRoutes.Screen
+            name="Confirmation"
+            component={Confirmation}
+        />
+        <StackRoutes.Screen
+            name="PlantSelect"
+            component={PlantSelect}
+        />
+        <StackRoutes.Screen
+            name="PlantSave"
+            component={PlantSave}
+        />
+        <StackRoutes.Screen
+            name="MyPlants"
+            component={MyPlants}
+        />
+
+    </StackRoutes.Navigator>
+)
+
+export default AppRoutes;
